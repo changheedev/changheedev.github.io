d57585147abfbab198d6bc223a37fdcffe522c28
Changehee Choi <changhee.dev@gmail.com>
2020-12-30 21:55:38 +0900

    Update theme files

---
+++ _tabs/about.md
@@ -0,0 +1,28 @@
+---
+title: About
+icon: fas fa-info
+order: 4
+# The About page
+# v2.0
+# https://github.com/cotes2020/jekyll-theme-chirpy
+# © 2017-2019 Cotes Chung
+# MIT License
+---
+
+### Education 👨‍💻
+
+- 네이버 커넥트재단 부스트캠프 2020 멤버십 - 웹 풀스택 (수료)
+
+- 네이버 커넥트재단 부스트캠프 2020 챌린지 - 웹 풀스택 (수료)
+
+- 경북대학교 컴퓨터학부 복수전공 (졸업)
+
+### Interest 👀
+
+- Web BackEnd, DevOps
+
+- Java, Spring, Node.js, Docker
+
+### Contacts 📫
+
+- Email: [changhee.dev@gmail.com](mailto:changhee.dev@gmail.com)
--- assets/js/_commons/back-to-top.js
+++ assets/js/_commons/back-to-top.js
@@ -1,18 +1,18 @@
 /*
 Reference: https://bootsnipp.com/snippets/featured/link-to-top-page
 */
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 50
-      && $("#sidebar-trigger").css("display") === "none") {
-    $("#back-to-top").fadeIn();
-  } else {
-    $("#back-to-top").fadeOut();
-  }
-});
-
 $(function() {
-  $("#back-to-top").click(function() {
-    $("body,html").animate({scrollTop: 0}, 800);
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 50 &&
+      $("#sidebar-trigger").css("display") === "none") {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
+  });
+
+  $("#back-to-top").click(() => {
+    $("body,html").scrollTop(0);
     return false;
   });
 });
